8b779e2fb52e861d7381ae21aa51fe7314246108
MichalS16 <michal.smiesko@seznam.cz>
2025-01-04 22:28:21 +0100

    deploy prep

--- pages/2_👨‍💼_Politician_Finder.py
+++ pages/2_👨‍💼_Politician_Finder.py
@@ -29,7 +29,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="auto",
     menu_items={
-        'About': "### US politicians Trading visualization\n An interactive web application visualizing U.S. Senators' financial trading activities, analyzing potential insider trading, and offering portfolio-based recommendations.\n\n *Made by Dario Mikuš and Michal Smieško*"
+        'About': "### US politicians Trading visualization\n An interactive web application visualizing U.S. Senators' financial trading activities, analyzing potential insider trading, and offering portfolio-based recommendations."
     }
 )
 
